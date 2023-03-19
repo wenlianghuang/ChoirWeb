6afcb9c11dd431facaf91e84f21e2efec0eda0da
wenlianghuang <wenliangmatt@gmail.com>
2023-03-19 12:10:14 +0800

    test3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, I think its correct
+          Learn React, No, its not.
         </a>
       </header>
     </div>
